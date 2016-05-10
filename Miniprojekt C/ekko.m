e5ad52af8646a17944970f27be22df840451a5e2
201409554 <201409554@post.au.dk>
2016-05-10 12:58:02 +0200

    Mmmh lort

--- Miniprojekt C/ekko.m
+++ Miniprojekt C/ekko.m
@@ -1,16 +1,31 @@
-function b = ekko(amplitude)
+function s_filtered_voice_ekko = ekko(s_voice ,amplitude, ekko)
 
 
 fsample = 44100;
 
-
-ekko_sekunder = 0.1;
-
-
-
 % ***** Ekko-filter ****************************************************
-delay_samples = round(ekko_sekunder*fsample);
+delay_samples = round(ekko*fsample);
  
  b = [amplitude zeros(1,delay_samples)];
+
+  % ***** Frekvensanalyse ************************************************
+H = fft(b,fsample);
+
+% ***** Plots **********************************************************
+%figure(2); clf
+%semilogx(20*log10(abs(H(1:0.5*fsample))))
+%figure(1);clf
+%plot(b)
+
+% ***** Filtrering på lyd **********************************************,
+
+s_voice_left = s_voice(:,1)';  % venstre kanal i wave-filen udtages
+
+s_filtered_voice = filter(b,1,s_voice_left);  % filtrering med b (FIR)
+
+
+% ***** filtreret version ***************************
+s_filtered_voice_ekko = s_filtered_voice/max(abs(s_filtered_voice));  % normalisering
+%soundsc(s_filtered_voice_norm, fsample)
  
 end
